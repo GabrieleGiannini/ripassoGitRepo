2fdd038bd995e7c9e54cc9bf5aec2def833802d5
Gabriele Giannini <gabrielegiannini2@gmail.com>
2017-03-18 15:01:54 +0100

    prova git ignore

---
+++ provaGitRepo/src/main/java/tap/giannini/provaGitRepo/App.java
@@ -0,0 +1,13 @@
+package tap.giannini.provaGitRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
